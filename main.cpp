5d2e64e8f301710dfae7b0ad08ad0dc48db2f54c
ChungHyun Lee <chunghyun.dev@gmail.com>
2025-01-28 00:19:01 +0900

    baekjoon: 14469

--- main.cpp
+++ main.cpp
@@ -1,26 +1,17 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-int n, a, b, ret; 
-vector<pair<int, int>> v;
-priority_queue<int, vector<int>, greater<int>> pq; 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+#include<bits/stdc++.h>
+
+using namespace std;
+int n;
+int main(){
 	cin >> n; 
-	for(int i = 0; i < n; i++){
-		cin >> a >> b; 
-		v.push_back({a, b}); 
-	}
-	sort(v.begin(), v.end());
-    for(int i = 0; i < v.size(); i++){
-        ret += v[i].second;
-        pq.push(v[i].second);
-        if(v[i].first < pq.size()){
-            ret -= pq.top();
-            pq.pop();
-        }
+	vector<pair<int,int>> a(n);
+	for(int i =0; i < n; i++) cin >> a[i].first >> a[i].second;
+	sort(a.begin(),a.end()); 
+	int realTime = a[0].first + a[0].second;
+	for(int i = 1; i < a.size(); i++){
+        realTime = max(realTime, a[i].first);
+        realTime += a[i].second;
     }
-    cout << ret;
+	cout << realTime << "\n"; 
     return 0;
 }
