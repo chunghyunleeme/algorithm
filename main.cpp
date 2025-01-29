790b9f2a71588fb43ce138a851bffc95ed3b56f7
ChungHyun Lee <chunghyun.dev@gmail.com>
2025-01-29 15:17:36 +0900

    baekjoon: 1931

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-int n;
+int from, to, n, idx = 0, ret = 1;  
 int main(){
-	cin >> n; 
-	vector<pair<int,int>> a(n);
-	for(int i =0; i < n; i++) cin >> a[i].first >> a[i].second;
-	sort(a.begin(),a.end()); 
-	int realTime = a[0].first + a[0].second;
-	for(int i = 1; i < a.size(); i++){
-        realTime = max(realTime, a[i].first);
-        realTime += a[i].second;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    vector<pair<int, int>> v; 
+    for(int i = 0; i < n; i++){
+        cin >> from >> to;
+        v.push_back({to, from});
     }
-	cout << realTime << "\n"; 
+    sort(v.begin(), v.end());
+    from = v[0].second;
+    to = v[0].first;
+    for(int i = 1; i < n; i++){
+        if(v[i].second < to) continue;
+        from = v[i].second; to = v[i].first; ret++;
+    }
+    cout << ret;
     return 0;
 }
